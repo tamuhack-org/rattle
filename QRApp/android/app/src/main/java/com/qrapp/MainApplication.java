49328c58786128de70a4acbc888d9b0e8867a720
amangotchu <aman.gottu@gmail.com>
2019-07-29 20:52:18 -0700

    Removed auto signing

--- QRApp/android/app/src/main/java/com/qrapp/MainApplication.java
+++ QRApp/android/app/src/main/java/com/qrapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.qrapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AsyncStoragePackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNCameraPackage()
