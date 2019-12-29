c752fc4335192256f3a3204ef9c238a254dae1fc
amangotchu <aman.gottu@gmail.com>
2019-12-29 12:35:26 -0600

    Updates

---
+++ precache-manifest.c9dd9cf8df3883965d22b9af26e5c6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac7ce294b33c8af58b0c4860ad7f505",
+    "url": "/rattle/index.html"
+  },
+  {
+    "revision": "9ef6133f45edcce39760",
+    "url": "/rattle/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9ef6133f45edcce39760",
+    "url": "/rattle/static/js/2.d41cbe36.chunk.js"
+  },
+  {
+    "revision": "83fc478e6c2fc7249d86",
+    "url": "/rattle/static/js/main.3873b041.chunk.js"
+  },
+  {
+    "revision": "079f0f1706345039b055",
+    "url": "/rattle/static/js/runtime-main.f77972af.js"
+  },
+  {
+    "revision": "768b0a92efdcfb6edae3064f57911edc",
+    "url": "/rattle/static/media/hiss.768b0a92.svg"
+  }
+]);
