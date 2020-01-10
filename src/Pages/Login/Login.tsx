6dfdc007c4c82f0c70de4fbaf4d75f4384dcd0e1
= <nathanreddycode@gmail.com>
2020-01-10 17:40:07 -0600

    Fix error

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -93,7 +93,9 @@ class Login extends React.PureComponent<IProps, IState> {
     const { redirectToSelection } = this.state;
 
     if(redirectToSelection) {
-      <Redirect to='/select'/>
+      return (
+        <Redirect to='/select'/>
+      )
     }
  
     // Create the success and failure toast
